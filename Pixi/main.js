666ec9544f32079e29ee498fe00541ab93d1b6c3
Alex Tan <torc.madra@hotmail.com>
2017-09-27 17:48:09 +1000

    Added birthday game link

--- Pixi/main.js
+++ Pixi/main.js
@@ -12,8 +12,8 @@ var frameCount = 0;
 
 function initPixi() {
 	var type = "WebGL";
-	if(!PIXI.utils.isWebGLSupported()){
-	  type = "canvas";
+	if (!PIXI.utils.isWebGLSupported()) {
+		type = "canvas";
 	}
 	PIXI.utils.sayHello(type);
 	renderer = PIXI.autoDetectRenderer(800, 450);
@@ -37,7 +37,7 @@ var graphics = new PIXI.Graphics();
 stage.addChild(graphics);
 
 // events
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
 	if (document.activeElement !== document.getElementById("canvas")) {
 		return;
 	}
@@ -52,7 +52,7 @@ document.addEventListener("keydown", function(e) {
 	}
 });
 
-document.getElementById("canvas").addEventListener("wheel", function(e) {
+document.getElementById("canvas").addEventListener("wheel", function (e) {
 	if (e.deltaY < 0) {
 		zoomIn();
 	} else if (e.deltaY > 0) {
@@ -60,9 +60,9 @@ document.getElementById("canvas").addEventListener("wheel", function(e) {
 	}
 });
 
-document.getElementById("canvas").addEventListener("mousemove", function(e) {
+document.getElementById("canvas").addEventListener("mousemove", function (e) {
 	return;
-	if (e.buttons%2 === 1) {
+	if (e.buttons % 2 === 1) {
 		var p = argand.calculateMouseCoordinate(e);
 		var z = math.complex(0, 0);
 		for (var i = 0; i < 100; i++) {
@@ -71,9 +71,9 @@ document.getElementById("canvas").addEventListener("mousemove", function(e) {
 				c: p
 			};
 			z = math.complex(fn.eval(scope));
-			var clr = Number("0x"+colorsys.hsv_to_hex(50*p.abs(),100,100).substring(1));
-			argand.addCoordinate({point: z, color: clr});
-		}	
+			var clr = Number("0x" + colorsys.hsv_to_hex(50 * p.abs(), 100, 100).substring(1));
+			argand.addCoordinate({ point: z, color: clr });
+		}
 	}
 
 });
@@ -91,65 +91,79 @@ var speed = 100;
 var delta = 0.05;
 var fn = null;
 
+var done = false;
+
 var argand = new Argand();
 
 function updateGui() {
-	
+
 	document.getElementById("btn_incrloop").value = "Increase loop limit [" + Math.round(loopLimit) + "]";
 	document.getElementById("btn_decrloop").value = "Decrease loop limit [" + Math.round(loopLimit) + "]";
 	document.getElementById("btn_incrbounds").value = "Increase bounds [" + bounds.toFixed(4) + "]";
 	document.getElementById("btn_decrbounds").value = "Decrease bounds [" + bounds.toFixed(4) + "]";
 	document.getElementById("btn_incrdelta").value = "Increase delta [" + delta.toFixed(4) + "]";
 	document.getElementById("btn_decrdelta").value = "Decrease delta [" + delta.toFixed(4) + "]";
-	
+
 	document.getElementById("btn_zoomin").value = "Zoom in [" + Math.round(argand.zoom) + "]";
 	document.getElementById("btn_zoomout").value = "Zoom out [" + Math.round(argand.zoom) + "]";
 	document.getElementById("btn_incrspeed").value = "Increase speed [" + Math.round(speed) + "]";
 	document.getElementById("btn_decrspeed").value = "Decrease speed [" + Math.round(speed) + "]";
+
+	document.getElementById("txtbx_formula").addEventListener("keypress", function (e) {
+		if (e.keyCode == 13) {
+			document.getElementById("btn_render").onclick();
+		}
+	});
 }
 
 function gameLoop() {
 	requestAnimationFrame(gameLoop);
-	
+
 	updateGui();
-	
-	for (var k = 0; k < speed; k++) {
-		z = mandelbrot(z, math.complex(i, j), fn);
-		loopCtr++;
 
-		if (loopCtr >= math.floor(loopLimit)) {
-			loopCtr = 0;
-			z = math.complex(0, 0);
+	if (!done) {
+		for (var k = 0; k < speed; k++) {
+			z = mandelbrot(z, math.complex(i, j), fn);
+			loopCtr++;
+			if (done) break;
 
-			/*if (i < bounds) {
-				i *= -1;
-				if (i === 0) {
+			if (loopCtr >= math.floor(loopLimit)) {
+				loopCtr = 0;
+				z = math.complex(0, 0);
+
+				/*if (i < bounds) {
+					i *= -1;
+					if (i === 0) {
+						i += delta;
+					} else {
+						i += math.sign(i)*delta;
+					}
+				} else if (j < bounds) {
+					j *= -1;
+					if (j === 0) {
+						j += delta;
+					} else {
+						j += math.sign(j)*delta;
+					}
+					i = 0;
+				}*/
+
+				if (i < bounds) {
 					i += delta;
-				} else {
-					i += math.sign(i)*delta;
-				}
-			} else if (j < bounds) {
-				j *= -1;
-				if (j === 0) {
+				} else if (j < bounds) {
 					j += delta;
-				} else {
-					j += math.sign(j)*delta;
+					i = -bounds;
 				}
-				i = 0;
-			}*/
-			
-			if (i < bounds) {
-				i += delta;
-			} else if (j < bounds) {
-				j += delta;
-				i = -bounds;
-			}
 
+			}
+			if (i >= loopLimit && j >= loopLimit) {
+				done = true;
+			}
 		}
 	}
-	
+
 	argand.render();
-	
+
 	renderer.render(stage);
 	frameCount++;
 }
@@ -161,21 +175,21 @@ function mandelbrot(z, c, fn) {
 	};
 	var zn = math.complex(fn.eval(scope));
 	var p = math.complex(i, j);
-	//var clr = Number("0x"+colorsys.hsv_to_hex(100*p.abs(),100,100).substring(1));
-	var clr = Number("0x"+colorsys.hsv_to_hex(100*p.abs(),0,100).substring(1));
+	var clr = Number("0x" + colorsys.hsv_to_hex(100 * p.abs(), 100, 100).substring(1));
+	//var clr = Number("0x"+colorsys.hsv_to_hex(100*p.abs(),0,100).substring(1));
 	//var alpha = (1+loopCtr)/(1+loopLimit);
-	var alpha = 0;
-	if (loopCtr >= loopLimit - 2) {
-		alpha = 1;
-	} else {
-		alpha = 0;
-	}
-	argand.addCoordinate({point: zn, color: clr, alpha: alpha});
+	var alpha = 0.8;
+	// if (loopCtr >= loopLimit - 2) {
+	// 	alpha = 1;
+	// } else {
+	// 	alpha = 0;
+	// }
+	argand.addCoordinate({ point: zn, color: clr, alpha: alpha });
 	return zn;
 }
 
 function collatz(z) {
-	if (z.re%2===0 && z.im%2===0) {
+	if (z.re % 2 === 0 && z.im % 2 === 0) {
 		z = z.div(2);
 	} else {
 		z = z.mul(3);
@@ -186,20 +200,25 @@ function collatz(z) {
 
 function increase(x) {
 	var n = x * 1.6;
-	if (n-x<1) {
-		n=x+1;
+	if (n - x < 1) {
+		n = x + 1;
 	}
 	return n;
 }
 function decrease(x) {
 	var n = x / 1.6;
-	if (x-n<1) {
-		n=x-1;
+	if (x - n < 1) {
+		n = x - 1;
 	}
-	if (n<1) {
-		n=1;
+	if (n < 1) {
+		n = 1;
 	}
 	return n;
+}
+
+function clearScreen() {
+	argand.clear();
+	done = true;
 }
 
 function reset() {
@@ -208,6 +227,7 @@ function reset() {
 	i = -bounds;
 	j = -bounds;
 	loopCtr = 0;
+	done = false;
 }
 
 function increaseSpeed() {
@@ -236,7 +256,7 @@ function decreaseBounds() {
 
 function increaseDelta() {
 	delta *= 1.6;
-}	
+}
 
 function decreaseDelta() {
 	delta /= 1.6;
