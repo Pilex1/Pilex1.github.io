57496a365e09ecf52991f038552be00796d8dfda
Alex Tan <torc.madra@hotmail.com>
2017-10-14 14:02:00 +1100

    Text wraps around images in Xing

--- helper.js
+++ helper.js
@@ -1,6 +1,5 @@
 function setActive(title, description) {
     $("header>h1").html(title);
-    console.log($("header>h1"));
     $(".main-nav>ul>li>a:contains('"+title+"')").attr("class", "header_current_heading");
     $("header>p").html(description);
 }
