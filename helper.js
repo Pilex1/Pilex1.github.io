af5234797f9853b2540f5e28f45d154b438789ad
Alex Tan <torc.madra@hotmail.com>
2018-09-03 15:26:34 +1000

    updated styling

--- helper.js
+++ helper.js
@@ -1,5 +1,4 @@
 function setActive(title, description) {
     $("header>h1").html(title);
-    $(".main-nav>ul>li>a:contains('"+title+"')").attr("class", "header_current_heading");
     $("header>p").html(description);
 }
